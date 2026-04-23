892bfd604f7be8866eade6a32768cb0f889c20c1
dravid <dravidmani1998@gmail.com>
2026-04-23 23:12:16 +0530

    Remove unwanted import

--- src/components/MainSection/Contact.tsx
+++ src/components/MainSection/Contact.tsx
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import {
-  TextField,
-  Button,
-  useTheme,
-  IconButton,
-  Typography,
-  Box,
-} from "@mui/material";
+import { TextField, Button, useTheme, Typography, Box } from "@mui/material";
 import { Formik } from "formik";
 import emailjs from "@emailjs/browser";
 
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import SendIcon from "@mui/icons-material/Send";
 
 const Section = styled.section`
